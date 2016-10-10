14f5a8b883e5917f4c66318207bf8af0e551f98d
Mario Wenzel <maweki@gmail.com>
2016-10-10 22:08:12 +0200

    mehr Auswertung

--- convert.py
+++ convert.py
@@ -59,7 +59,58 @@ def pdf_data_to_row(data):
     return [
     check(data['Markierfeld 1']), check(data['Markierfeld 2']), check(data['Markierfeld 3']), check(data['Markierfeld 4']), check(data['Markierfeld 5']), check(data['Markierfeld 6']), check(data['Markierfeld 7']), data['Textfeld 1'],
     check(data['Markierfeld 9']), check(data['Markierfeld 10']), check(data['Markierfeld 11']), check(data['Markierfeld 12']), check(data['Markierfeld 13']),
-    'q2c', 'q2c1', 'q2c2', 'q2c3', 'q2d', 'q2e', 'q2e1', 'q2e2', 'q2e3', 'q2e4', 'q3a', 'q3b', 'q3c', 'q3d', 'q4a', 'q4b', 'q4b1', 'q4b2', 'q4b3', 'q4b4', 'q4b5', 'q4b6', 'q4b7', 'q4b8', 'q5a', 'q5b', 'q5c', 'q5d', 'q5e', 'q5f', 'q6', 'q7a', 'q7b', 'q7c', 'q7d', 'q7e', 'q7f', 'q7g', 'q7h', 'q7i', 'q7j', 'q7k', 'q7l', 'q7m', 'q7n', 'q7o', 'q7p', 'q7q', 'q7r', 'q7s', 'q7t', 'q7u', 'q7v', 'q7w', 'q7x', 'q7y', 'q8a', 'q8b', 'q8c', 'q8d', 'q8e', 'q8f', 'q8g', 'q8h', 'q9', 'q10a', 'q10b', 'q10c', 'q10d', 'q10e', 'q10f', 'q10g', 'q10h', 'q10i', 'q10j', 'q10k', 'q10l', 'q10m', 'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17a', 'q17b', 'q17c', 'q17d', 'q17e', 'q17f', 'q18', 'q19a', 'q19b', 'q19c', 'q19c1', 'q19c2', 'q19c3', 'q19c4', 'q20a', 'q20b', 'q20c', 'q20d', 'q20e', 'q20f', 'q20g', 'q20g1', 'q20g2', 'q20g4', 'q20g5', 'q20g6', 'q20g7', 'q21a', 'q21b', 'q21c', 'q21d', 'q21e', 'q21f', 'q21g', 'q21h', 'q21i', 'q21i1', 'q22', 'q22a', 'q23a', 'q23b', 'q23c', 'q23d', 'q24', 'q25', 'q26', 'q27', 'q28', 'q29', 'q30', 'q31', 'q32', 'q33', 'q34', 'q35', 'q36', 'q37', 'q38', 'q39', 'q40', 'q41', 'q42', 'q43', 'q44', 'q45', 'q46', 'q47'
+    'q2c', 'q2c1', 'q2c2', 'q2c3', 'q2d', 'q2e', 'q2e1', 'q2e2', 'q2e3', data['Textfeld 1'],
+    'q3a', 'q3b', 'q3c', 'q3d',
+    'q4a', 'q4b', 'q4b1', 'q4b2', 'q4b3', 'q4b4', 'q4b5', 'q4b6', 'q4b7', 'q4b8',
+    'q5a', 'q5b', 'q5c', 'q5d', 'q5e', 'q5f',
+    'q6',
+    'q7a', 'q7b', 'q7c', 'q7d', 'q7e', 'q7f', 'q7g', 'q7h', 'q7i', 'q7j', 'q7k', 'q7l', 'q7m', 'q7n', 'q7o', 'q7p', 'q7q', 'q7r', 'q7s', 'q7t', 'q7u', 'q7v', 'q7w', 'q7x', 'q7y',
+    'q8a', 'q8b', 'q8c', 'q8d', 'q8e', 'q8f', 'q8g', 'q8h',
+    'q9',
+    'q10a', 'q10b', 'q10c', 'q10d', 'q10e', 'q10f', 'q10g', 'q10h', 'q10i', 'q10j', 'q10k', 'q10l', 'q10m',
+    'q11',
+    'q12',
+    'q13',
+    'q14',
+    'q15',
+    'q16',
+    'q17a', 'q17b', 'q17c', 'q17d', 'q17e', 'q17f',
+    'q18',
+    'q19a', 'q19b', 'q19c', 'q19c1', 'q19c2', 'q19c3', 'q19c4',
+    # informationen
+    'q20a', 'q20b', 'q20c', 'q20d', 'q20e', 'q20f', 'q20g', 'q20g1', 'q20g2', 'q20g4', 'q20g5', 'q20g6', data['Textfeld 17'],
+    # informationen
+    check(data['Markierfeld 128']), check(data['Markierfeld 129']), check(data['Markierfeld 130']), check(data['Markierfeld 131']), check(data['Markierfeld 132']), check(data['Markierfeld 133']), check(data['Markierfeld 134']), check(data['Markierfeld 135']), check(data['Markierfeld 136']), data['Textfeld 18'],
+    check(data['Markierfeld 137']), data['Textfeld 6'],
+    # engagement
+    check(data['Markierfeld 139']), check(data['Markierfeld 140']), check(data['Markierfeld 141']), data['Textfeld 19'],
+    # Zufriedenheit Partei
+    num(data['Textfeld 20']),
+    num(data['Textfeld 21']),
+    num(data['Textfeld 22']),
+    num(data['Textfeld 23']),
+    num(data['Textfeld 24']),
+    num(data['Textfeld 25']),
+    # Zufriedenheit Parlament
+    num(data['Textfeld 26']),
+    num(data['Textfeld 27']),
+    num(data['Textfeld 28']),
+    num(data['Textfeld 29']),
+    num(data['Textfeld 30']),
+    # Schluss
+    num(data['Textfeld 31']),
+    'q36', # geschlecht
+    data['Textfeld 32'],
+    num(data['Textfeld 33']),
+    num(data['Textfeld 34']),
+    num(data['Textfeld 35']),
+    sel(data['abschl']),
+    num(data['Textfeld 36']),
+    data['Textfeld 37'],
+    data['Textfeld 38'],
+    data['Textfeld 39'],
+    data['Textfeld 40'],
+    data['Textfeld 41']
     ]
 
 def read_pdf(filename):
